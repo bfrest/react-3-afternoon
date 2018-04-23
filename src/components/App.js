89ea91c774e262498d80b4063a897ea40e3ed086
Brendon Freston <freston75@gmail.com>
2018-04-23 07:10:47 -0600

    finished all but the black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -21,16 +21,28 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get("https://practiceapi.devmountain.com/api").then(results => {
+    axios.get("https://practiceapi.devmountain.com/api/posts").then(results => {
       this.setState({ posts: results.data });
     });
   }
 
-  updatePost() {}
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text }).then(results => {
+      this.setState({ posts: results.data });
+    });
+  }
 
-  deletePost() {}
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(results => {
+      this.setState({ posts: results.data });
+    });
+  }
 
-  createPost() {}
+  createPost(text) {
+    axios.post("https://practiceapi.devmountain.com/api/posts", { text }).then(results => {
+      this.setState({ posts: results.data });
+    });
+  }
 
   render() {
     const { posts } = this.state;
@@ -40,11 +52,8 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-          <Compose />
-          {posts.map(post => {
-            <Post key={post.id} />;
-          })}
-          <Post />
+          <Compose createPostFn={this.createPost} />
+          {posts.map(post => <Post key={post.id} text={post.text} date={post.date} id={post.id} updatePostFn={this.updatePost} deletePostFn={this.deletePost} />)}
         </section>
       </div>
     );
